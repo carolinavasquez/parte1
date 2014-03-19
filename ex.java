baee710c942d2ed968e2561b5573de452d6f3f28
Alumno <alumno@localhost.localdomain>
2014-03-19 09:14:16 -0600

    first commit

---
+++ ex.java
@@ -0,0 +1,71 @@
+package deysicarol;
+
+import java.util.Scanner;
+
+public class ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println ("Introduce el 1 numero:");
+		Scanner stdin=new Scanner (System.in);
+		double calif1=stdin.nextDouble();
+		System.out.println ("Introduce el 2 numero:");
+		double calif2=stdin.nextDouble();
+		System.out.println ("Introduce el 3 numero:");
+		double calif3=stdin.nextDouble();
+		System.out.println ("Introduce el 4 numero:");
+		double calif4=stdin.nextDouble();
+		System.out.println ("Introduce el 5 numero:");
+		double calif5=stdin.nextDouble();
+		System.out.println ("Introduce el 6 numero:");
+		double calif6=stdin.nextDouble();
+		System.out.println ("Introduce el 7 numero:");
+		double calif7=stdin.nextDouble();
+		System.out.println ("Introduce el 8 numero:");
+		double calif8=stdin.nextDouble();
+		System.out.println ("Introduce el 9 numero:");
+		double calif9=stdin.nextDouble();
+		System.out.println ("Introduce 10 numero:");
+		double calif10=stdin.nextDouble();
+		System.out.println ("Introduce el 11 numero:");
+		double calif11=stdin.nextDouble();
+		System.out.println ("Introduce el 12 numero:");
+		double calif12=stdin.nextDouble();
+		System.out.println ("Introduce el 13 numero:");
+		double calif13=stdin.nextDouble();
+		System.out.println ("Introduce el 14 numero:");
+		double calif14=stdin.nextDouble();
+		System.out.println ("Introduce el 15 numero:");
+		double calif15=stdin.nextDouble();
+		System.out.println ("Introduce el 16 numero:");
+		double calif16=stdin.nextDouble();
+		System.out.println ("Introduce el 17 numero:");
+		double calif17=stdin.nextDouble();
+		System.out.println ("Introduce el 18 numero:");
+		double calif18=stdin.nextDouble();
+		System.out.println ("Introduce el 19 numero:");
+		double calif19=stdin.nextDouble();
+		System.out.println ("Introduce el 20 numero:");
+		double calif20=stdin.nextDouble();
+		
+		double promedio=stdin.nextDouble();
+		for(int i = 1;i<=20;++i)
+		promedio=((calif1+calif2+calif3
+		+calif4+calif5+calif6+calif7+calif8+calif9+calif10+calif11+calif12+calif13+calif14+calif15+calif16+calif17+calif18+calif19+calif20)/20);
+		if (promedio>6){
+	
+		}
+		
+		else
+		System.out.println("");
+
+
+		}
+
+
+		
+		
+	
+	}
+
+}
